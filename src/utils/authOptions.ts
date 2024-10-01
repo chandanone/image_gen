348aaf577b863c564c1a2bfe28fedfcfb1978b9e
chandan <chandan868@gmail.com>
2024-10-01 17:35:34 +0530

    session issue resolved

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,29 +1,27 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import GoogleProvider from "next-auth/providers/google"
-import prisma from "./prisma"
-import { NextAuthOptions } from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./prisma";
+import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
-  
-  adapter: PrismaAdapter(prisma),  
+  adapter: PrismaAdapter(prisma),
   providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-      ],
-      callbacks: {
-        async session({session}) {
-          const user = await prisma.user.findUnique({
-            where: { email: session.user?.email || ""}
-          });
-          if(user){
-            session.user = {
-              ...user,
-            };
-            return session;
-          }
-        }
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const user = await prisma.user.findUnique({
+        where: { email: session.user?.email || "" },
+      });
+      if (user) {
+        session.user = {
+          ...user,
+        };
       }
-      
-}
+      return session;
+    },
+  },
+};
